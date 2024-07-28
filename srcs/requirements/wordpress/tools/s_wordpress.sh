9e215d2ae88215095ec10e1bf661a2929ef6d9e0
El Mehdi Naam <enaam@e3r3p9.1337.ma>
2024-07-28 22:24:21 +0100

    finale

--- srcs/requirements/wordpress/tools/s_wordpress.sh
+++ srcs/requirements/wordpress/tools/s_wordpress.sh
@@ -3,7 +3,7 @@
 # Download and set up WordPress
 wp core download --allow-root
 wp config create --dbname="$MARIA_DATABS" --dbuser="$MARIA_USER" --dbpass="$USER_PWD" --dbhost="$DB_HOST" --allow-root
-wp core install --url="enaam.42.fr" --title="Welcom Inception" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --allow-root
+wp core install --url="$DOMAINE_NAME" --title="Welcom Inception" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --allow-root
 wp config set FS_METHOD direct --allow-root
 # Create an additional WordPress user
 wp user create "$WP_USER" "$WP_EMAIL" --role=author --user_pass="$WP_PASSWORD"  --allow-root
